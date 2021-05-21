7fee67cebcdb852b7c100d28e93a40d21af7c98a
Artur Santos <artur_teixeira10@hotmail.com>
2021-05-21 09:37:30 -0300

    code

--- js/script.js
+++ js/script.js
@@ -9,12 +9,7 @@ function calcular() {
 
     let preco = (((Number(produto.value) * ((Number(lucro.value) / 100) + 1)) + Number(embalagem.value)) * ((Number(porcentagem_loja_virtual.value) / 100) + 1))
 
-    function ajuste(nr, casas) {
-        const og = Math.pow(10, casas)
-        return Math.floor(nr * og) / og;
-      }
-
-    p.innerHTML = `R$ ${ajuste(preco, 1) + '9'}`
+    p.innerHTML = `R$ ${preco.toFixed(1) + '9'}`
 
 }
 
@@ -30,14 +25,14 @@ function limpar() {
 }
 
 function limpar_all() {
-    
+
     let produto = document.getElementById('valor-de-custo')
     let lucro = document.getElementById('input-porcentagem')
     let embalagem = document.getElementById('input-embalagem')
     let porcentagem_loja_virtual = document.getElementById('input-loja-virtual')
 
     let p = document.getElementById('p')
-    
+
     produto.value = ''
     lucro.value = ''
     embalagem.value = ''
